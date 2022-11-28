6fc2ebf9f1d14e41a7769f8e05aa897fa0b694df
alejandrasolis611 <118215926+alejandrasolis611@users.noreply.github.com>
2022-11-27 18:02:35 -0600

    Update EsotilinProyecto.java

--- src/esotilin/proyecto/EsotilinProyecto.java
+++ src/esotilin/proyecto/EsotilinProyecto.java
@@ -17,6 +17,7 @@ public class EsotilinProyecto {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("ola carambolas");
+        System.out.println("hola"):
     }
     
 }
